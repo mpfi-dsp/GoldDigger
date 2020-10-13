6ca42dce562e00cfee96f97bc7373c6caf0a71dd
Misha <mikeusru@gmail.com>
2020-10-13 10:31:01 -0400

    changed imagefield to filefield for real this time

--- GDapp/models.py
+++ GDapp/models.py
@@ -4,7 +4,7 @@ from django import forms
 
 # Create your models here.
 class Profile(models.Model):
-    profile = models.ImageField(upload_to="Input/")
+    profile = models.FileField(upload_to="Input/")
 
 
 
