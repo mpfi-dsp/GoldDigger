4f796edba11ac1ede1abb0d3299647269e86afff
Misha <mikeusru@gmail.com>
2020-10-19 13:32:08 -0400

    put everything in run_docker

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker run -d --name redis1 redis
-docker run --gpus all --name gold-digger-web --link redis1:redis -p 8000:8000 -v ${PWD}:/usr/src/app gold-digger/gold-digger-dev sh -c "celery -A GoldDigger worker -l info"
-# docker exec -ti gold-digger-web sh -c "celery -A GoldDigger worker -l info"
+docker run --gpus all -d --name gold-digger-web --link redis1:redis -p 8000:8000 -v ${PWD}:/usr/src/app gold-digger/gold-digger-dev sh -c "celery -A GoldDigger worker -l info"
+docker exec -ti gold-digger-web sh -c "python3 manage.py runserver 0.0.0.0:8000"
