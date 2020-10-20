b63f1b8dc3a08f5f91603564b814b69e54e0db56
Misha <mikeusru@gmail.com>
2020-10-20 14:01:30 -0400

    clean upload form now based on a modelform which saved data correctly

--- GDapp/models.py
+++ GDapp/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django import forms
 
+TRAINED_MODEL_CHOICES = [
+    ('43kGoldDigger', '43kGoldDigger'),
+    ('87kGoldDigger', '87kGoldDigger')
+]
 
-# Create your models here.
 class EMImage(models.Model):
     image = models.FileField(upload_to="Input/")
+    trained_model = models.CharField(max_length=100,
+                                     blank=False,
+                                     default='87kGoldDigger',
+                                     choices=TRAINED_MODEL_CHOICES)
