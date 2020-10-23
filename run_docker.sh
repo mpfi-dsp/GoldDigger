1156e07aa25f7fcb899ae6e51b5be9e3bf54b108
Misha <mikeusru@gmail.com>
2020-10-23 11:11:58 -0400

    update run_docker.sh file to remove existing containers before starting them

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+docker stop redis1
+docker stop gold-digger-web
+docker rm redis1
+docker rm gold-digger-web 
+
 docker run -d --name redis1 redis
 docker run --gpus all -d --name gold-digger-web --link redis1:redis -p 8000:8000 -v ${PWD}:/usr/src/app gold-digger/gold-digger-dev sh -c "celery -A GoldDigger worker -l info"
 docker exec -ti gold-digger-web sh -c "python3 manage.py makemigrations"
