1f5f4185883c80c6b7dd2a4a0a8a5cb8e01f8c66
mikeusru <mikeusru@gmail.com>
2020-10-23 12:24:29 -0400

    Update models.py

--- GDapp/models.py
+++ GDapp/models.py
@@ -8,10 +8,7 @@ TRAINED_MODEL_CHOICES = [
 
 class EMImage(models.Model):
     image = models.FileField(upload_to="Input/")
-<<<<<<< HEAD
     mask = models.FileField(upload_to="Mask/", blank=True)
-=======
->>>>>>> c9cc1c8e8f296c429e30f4b03ff400a355e61b45
     trained_model = models.CharField(max_length=100,
                                      blank=False,
                                      default='87kGoldDigger',
