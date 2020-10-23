6389efe58a23d99ee3bd52c02e8f45e9d0fbacb5
mikeusru <mikeusru@gmail.com>
2020-10-23 12:26:22 -0400

    Update forms.py

--- GDapp/forms.py
+++ GDapp/forms.py
@@ -6,8 +6,4 @@ class EMImageForm(forms.ModelForm):
 
     class Meta:
         model = EMImage
-<<<<<<< HEAD
         fields = ['image', 'mask', 'trained_model']
-=======
-        fields = ['image', 'trained_model']
->>>>>>> c9cc1c8e8f296c429e30f4b03ff400a355e61b45
