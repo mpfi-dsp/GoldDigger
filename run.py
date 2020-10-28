fe0dfce2d8dbbfa5e072e33d35fe2fba9b9461a5
Misha <mikeusru@gmail.com>
2020-10-28 16:58:51 -0400

    remove handleRemoveReadOnly func"

--- run.py
+++ run.py
@@ -28,15 +28,6 @@ import shutil
 # from sklearn.cluster import Kmeans
 
 
-def handleRemoveReadonly(func, path, exc):
-    excvalue = exc[1]
-    if func in (os.rmdir, os.remove) and excvalue.errno == errno.EACCES:
-        os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)  # 0777
-        func(path)
-    else:
-        raise
-
-
 def get_artifact_status(model):
     # gets rid of the constant artifact in the up left corner
     if model == '87kGoldDigger':
