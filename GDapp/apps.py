992748945b397f429852b011b8db768012cd8a64
Misha <mikeusru@gmail.com>
2020-10-29 13:03:31 -0400

    channels and celery running with gold digger

--- GDapp/apps.py
+++ GDapp/apps.py
@@ -1,5 +1,5 @@
 # from run import run_gold_digger
-from GDapp.tasks import celery_timer_task
+from GDapp.tasks import celery_timer_task, run_gold_digger_task
 from GDapp.prediction.FrontEndUpdater import FrontEndUpdater
 from django.apps import AppConfig
 from channels.layers import get_channel_layer
@@ -9,40 +9,40 @@ import threading
 
 channel_layer = get_channel_layer()
 
-def slow_response_function(pk):
-    time.sleep(3)
-    print('slow function done')
-    send_a_message(pk)
-    send_a_message(pk,'second message')
+# def slow_response_function(pk):
+#     time.sleep(3)
+#     print('slow function done')
+#     send_a_message(pk)
+#     send_a_message(pk,'second message')
 
-def celery_function(pk):
-    print(pk)
-    celery_timer_task.delay(pk)
+# def celery_function(pk):
+#     print(pk)
+#     celery_timer_task.delay(pk)
 
 
-def send_a_message(pk=None, message=None):
-    if pk is None:
-        pk = 158
-    if message is None:
-        message = 'this is a delayed message from apps.py!'
-    pk_group_name = "analysis_%s" % pk
-    async_to_sync(channel_layer.group_send)(
-        pk_group_name,
-        {
-            'type': 'analysis_message',
-            'message': message
-        })
+# def send_a_message(pk=None, message=None):
+#     if pk is None:
+#         pk = 158
+#     if message is None:
+#         message = 'this is a delayed message from apps.py!'
+#     pk_group_name = "analysis_%s" % pk
+#     async_to_sync(channel_layer.group_send)(
+#         pk_group_name,
+#         {
+#             'type': 'analysis_message',
+#             'message': message
+#         })
 
-def update_front_end_test(pk):
-    feu = FrontEndUpdater(pk)
-    feu.post_message('Front End Updater Updating')
+# def update_front_end_test(pk):
+#     feu = FrontEndUpdater(pk)
+#     feu.post_message('Front End Updater Updating')
 
-def test_slow_responder(pk):
-    x = threading.Thread(target=slow_response_function, args=[pk])
-    x.start()
+# def test_slow_responder(pk):
+#     x = threading.Thread(target=slow_response_function, args=[pk])
+#     x.start()
 
 class GdappConfig(AppConfig):
     name = 'GDapp'
     # gold_particle_detector = run_gold_digger
-    gold_particle_detector = celery_function
-    
+    # gold_particle_detector = celery_function
+    gold_particle_detector = run_gold_digger_task.delay
--- GDapp/views.py
+++ GDapp/views.py
@@ -11,24 +11,22 @@ from .models import EMImage
 
 import sys
 
-def start_gold_digger(obj):
-    if obj.mask.name == '':
-        mask = None
-    else:
-        mask = obj.mask.path
-    gold_digger = GdappConfig.gold_particle_detector
-    # front_end_updater = FrontEndUpdater(obj.id)
-    gold_digger(obj.id)
-    # gold_digger(obj.trained_model, obj.image.path, obj.particle_groups, mask, front_end_updater)
+# def start_gold_digger(obj):
+#     if obj.mask.name == '':
+#         mask = None
+#     else:
+#         mask = obj.mask.path
+#     gold_digger = GdappConfig.gold_particle_detector
+#     # front_end_updater = FrontEndUpdater(obj.id)
+#     gold_digger(obj.id)
+#     # gold_digger(obj.trained_model, obj.image.path, obj.particle_groups, mask, front_end_updater)
 
-# Create your views here.
 def home(request):
     return render(request, 'GDapp/home.html')
 
 def image_view(request):
     if request.method == 'POST':
         form = EMImageForm(request.POST, request.FILES)
-        
         if form.is_valid():
             instance = form.save()
             return run_gd(request, {'pk':instance.id})
@@ -38,12 +36,10 @@ def image_view(request):
 
 
 def run_gd(request, inputs):
-    obj = EMImage.objects.get(pk=inputs['pk'])
-    print('run task here')
-    celery_timer_task.delay(inputs['pk'])
-    print('after task')
-    # start_gold_digger(obj)
-    return render(request, 'GDapp/run_gd.html', {'pk':obj.id})
+    pk = inputs['pk']
+    gold_digger = GdappConfig.gold_particle_detector
+    gold_digger(pk)
+    return render(request, 'GDapp/run_gd.html', {'pk':pk})
 
 
 # attempt to make a downloadable csv
