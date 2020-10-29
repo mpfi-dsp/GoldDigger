764a864233e47e67b8d97cb431b2adf1a1321e1a
Misha <mikeusru@gmail.com>
2020-10-29 13:03:31 -0400

    channels and celery running with gold digger

--- GDapp/tasks.py
+++ GDapp/tasks.py
@@ -1,6 +1,9 @@
+from GDapp.models import EMImage
+from run import run_gold_digger
 from GDapp.prediction.FrontEndUpdater import FrontEndUpdater
 from celery import shared_task
 import time
+
 
 @shared_task(bind=True)
 def celery_timer_task(self, pk):
@@ -9,3 +12,14 @@ def celery_timer_task(self, pk):
     time.sleep(5)
     feu.update(10,"update from celery timer task")
     return ('Done')
+
+@shared_task(bind=True)
+def run_gold_digger_task(self, pk):
+    print(pk)
+    obj = EMImage.objects.get(pk=pk)
+    if obj.mask.name == '':
+        mask = None
+    else:
+        mask = obj.mask.path
+    front_end_updater = FrontEndUpdater(pk)
+    run_gold_digger(obj.trained_model, obj.image.path, obj.particle_groups, mask, front_end_updater)
