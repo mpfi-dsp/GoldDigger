ff3c8a49319f3d60d547af92f6d9d3e2d83f4823
Eleanor Stuart <stuarte@ds-linux-ws02>
2020-12-04 09:10:26 -0500

    fixed blue mask

--- scripts/run_django.sh
+++ scripts/run_django.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 docker exec -ti gold-digger-web sh -c "python3 manage.py makemigrations"
 docker exec -ti gold-digger-web sh -c "python3 manage.py migrate"
-docker exec -ti gold-digger-web sh -c "python3 manage.py runserver 0.0.0.0:8000"
+docker exec -ti gold-digger-web sh -c "python3 manage.py runserver 0.0.0.0:8888"
