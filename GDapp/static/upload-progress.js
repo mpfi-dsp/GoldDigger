2e4aae47b89275ca0c5d254769185f06e4b477ca
Misha <mikeusru@gmail.com>
2020-12-08 11:11:03 -0500

    added uploading message and cleaned up code

--- GDapp/static/upload-progress.js
+++ GDapp/static/upload-progress.js
@@ -3,14 +3,11 @@ const input = document.getElementById('id_image')
 const alertBox = document.getElementById('alert-box')
 const imageBox = document.getElementById('image-box')
 const progressBox = document.getElementById('progress-box')
-const cancelBox = document.getElementById('cancel-box')
-const cancelBtn = document.getElementById('cancel-btn')
 const csrf = document.getElementsByName('csrfmiddlewaretoken')
 const runBtn = document.getElementById('run-btn')
 
 runBtn.addEventListener('click', () => {
     progressBox.classList.remove('not-visible')
-    cancelBox.classList.remove('not-visible')
     const img_data = input.files[0]
     const fd = new FormData()
     fd.append('csrfmiddlewaretoken', csrf[0].value)
@@ -27,16 +24,18 @@ runBtn.addEventListener('click', () => {
         xhr: function () {
             const xhr = new window.XMLHttpRequest();
             xhr.upload.addEventListener('progress', e => {
-                // console.log(e)
                 if (e.lengthComputable) {
                     const percent = e.loaded / e.total * 100
                     console.log(percent)
+                    alertBox.innerHTML =    `<div class="alert alert-success" role="alert">
+                                                Uploading Images...
+                                            </div>`
                     progressBox.innerHTML = `<div class="progress">
-                                                <div class="progress-bar" role="progressbar" style="width: ${percent}%;" aria-valuenow="${percent}" aria-valuemin="0" aria-valuemax="100">25%</div>
-                                            </div>
-                                            <p>${percent.toFixed(1)}%</p>`
+                                                <div class="progress-bar" role="progressbar" style="width: ${percent}%;" aria-valuenow="${percent}" aria-valuemin="0" aria-valuemax="100">${percent.toFixed(1)}%</div>
+                                            </div>`
                 }
             })
+
             return xhr
 
         },
