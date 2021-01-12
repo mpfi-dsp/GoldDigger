7caaaa31dbd9d8cb14652664af020dd52beeaae6
Eleanor Stuart <stuarte@mpfi.org>
2021-01-12 12:49:00 -0500

    form labels test

--- GDapp/forms.py
+++ GDapp/forms.py
@@ -8,5 +8,6 @@ class EMImageForm(forms.ModelForm):
         model = EMImage
         widgets = {'preloaded_pk': forms.HiddenInput()}
         fields = ['trained_model', 'particle_groups', 'threshold_string', 'preloaded_pk']
+        labels = {'trained_model': 'Trained model', 'threshold_string': 'Cutoff string'}
 
         #fields = ['image', 'mask', 'trained_model', 'particle_groups', 'threshold_string']
