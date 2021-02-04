30766752f294bf42a32b16f2e070970cdb0452ab
Eleanor Stuart <stuarte@mpfi.org>
2021-02-04 12:45:26 -0500

    trying to make image name field in EMImage model

--- GDapp/models.py
+++ GDapp/models.py
@@ -43,7 +43,8 @@ class EMImage(models.Model):
     output_file = models.FileField(
         upload_to="analyzed/output", null=True)
 
-    imageName = pathlib.Path(image).stem
+    
+    imageName = pathlib.Path(image.path()).stem
 
     chunked_image_id = models.CharField(max_length=500, blank=True, default="")
     chunked_mask_id = models.CharField(max_length=500, blank=True, default="")
