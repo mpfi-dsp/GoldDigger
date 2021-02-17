867cd2199c0201293266b192e16cfc6ccdf7842c
Eleanor Stuart <stuarte@mpfi.org>
2021-02-17 14:29:43 -0500

    need to fix issue where all files in folder are given same pk

--- GDapp/views.py
+++ GDapp/views.py
@@ -175,6 +175,7 @@ def create_single_local_image_obj(form, local_files_form, image_path=None):
         obj.local_mask = local_files_form.cleaned_data["local_mask"]
     obj = populate_em_obj(obj, form)
     obj.save()
+    logger.debug(f"id: {obj.id}")
     return obj
 
 
