b23cc442aba45b3401022fda0638087daa35d9a8
Eleanor Stuart <stuarte@ds-linux-ws02>
2021-03-22 10:16:45 -0400

    change to config file

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 ALLOWED_HOSTS = [
     '0.0.0.0',
     '127.0.0.1',
-    '10.1.200.221', # host machine's local ip address
+    '10.202.1.192', # host machine's local ip address
     'localhost',
     ]
 
-LOCAL_IMAGE_FOLDER = "/home/mpfi.org/ds-lab-adm/Desktop/Drives/ds-prog/EM-DATA/gd-for-analysis"
+LOCAL_IMAGE_FOLDER = "/home/MPFI.ORG/stuarte/Desktop/Drives/ds-prog/EM-DATA/gd-for-analysis"
 
 VERSION_NUMBER = "1.02.01"
