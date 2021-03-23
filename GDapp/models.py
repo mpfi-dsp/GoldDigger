7e5c3bfdde4bd21aac981140fb65cae176994dfa
root <root@DESKTOP-Q8D52L7.localdomain>
2021-03-23 11:42:21 -0400

    changed time zone from UTC -version 1.04.05

--- GDapp/models.py
+++ GDapp/models.py
@@ -27,7 +27,7 @@ class EMImage(models.Model):
     mask = models.FileField(upload_to="Mask/", blank=True)
     threshold_string = models.CharField(max_length=200, blank=True, default="1, 60",
                                         help_text="Input comma-separated values to serve as the upper and lower boundaries for the area of each particle size.")
-    thresh_sens = models.FloatField(default = 4.)
+    thresh_sens = models.FloatField(default = 4., help_text="Use arrows or type in decimal values if desired.")
 
     # path for local_mask and local_images references location in the docker container, these are for local upload
     local_image = models.FilePathField(path='/usr/src/local-images', blank=True)
--- GoldDigger/settings.py
+++ GoldDigger/settings.py
@@ -142,8 +142,8 @@ LOGGING = {
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+#changed TIME_ZONE from UTC
+TIME_ZONE = 'America/Atikokan'
 
 USE_I18N = True
 
