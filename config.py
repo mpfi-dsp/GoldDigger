bcd9f7314b4729e250f0b6ed36a980a86075f8de
Eleanor Stuart <lnr.stuart@gmail.com>
2021-04-16 11:22:33 -0400

    display output with area labels as preview image - version 1.06.00

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ ALLOWED_HOSTS = [
 
 LOCAL_IMAGE_FOLDER = "/home/MPFI.ORG/stuarte/Desktop/Drives/ds-prog/EM-DATA/gd-for-analysis"
 
-VERSION_NUMBER = "1.05.05"
+VERSION_NUMBER = "1.06.00"
 
 DJANGO_DEV_PORT = "8001"
 
