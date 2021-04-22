ee2fe8a3ef79cf5be0b3642675033dbb4c5ab140
Eleanor Stuart <stuarte@mpfi.org>
2021-04-22 14:54:54 -0400

    commented out local_mask, made local_image directory upload only - version 1.07.07

--- GDapp/forms.py
+++ GDapp/forms.py
@@ -23,13 +23,13 @@ class LocalFilesForm(forms.Form):
     '''Files for local upload.'''
     # necessary to have this separate so it reloads correctly
     local_image = forms.FilePathField(**local_file_args)
-    local_mask = forms.FilePathField(**local_file_args)
+    #local_mask = forms.FilePathField(**local_file_args)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['local_image'] = forms.FilePathField(label='Local image file or folder of images and masks',
-                                                         **local_file_args, allow_folders=True)
-        self.fields['local_mask'] = forms.FilePathField(label='Local mask file (optional)',
-                                                        **local_file_args, allow_folders=False)
+                                                         **local_file_args, allow_folders=True, allow_files=False)
+        #self.fields['local_mask'] = forms.FilePathField(label='Local mask file (optional)',
+        #                                                **local_file_args, allow_folders=False)
         self.fields['local_image'].widget.attrs['class'] = 'form-control'
-        self.fields['local_mask'].widget.attrs['class'] = 'form-control'
+        #self.fields['local_mask'].widget.attrs['class'] = 'form-control'
