a8a9fa596f2dac95d268eff5920bf7aae4385f6b
Eleanor Stuart <stuarte@mpfi.org>
2021-04-22 13:07:04 -0400

    made previous runs button on progress bar page functional - version 1.07.06

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ ALLOWED_HOSTS = [
 
 LOCAL_IMAGE_FOLDER = "/home/MPFI.ORG/stuarte/Desktop/Drives/ds-prog/EM-DATA/gd-for-analysis"
 
-VERSION_NUMBER = "1.07.05"
+VERSION_NUMBER = "1.07.06"
 
 DJANGO_DEV_PORT = "8001"
 
