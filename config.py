ca56f2008f62c667ab4e870c35777c2fa31ebef7
Eleanor Stuart <stuarte@mpfi.org>
2021-05-11 11:58:42 -0400

    try to fix clear queue button by adding js function - version 1.08.03

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ ALLOWED_HOSTS = [
 
 LOCAL_IMAGE_FOLDER = "/home/MPFI.ORG/stuarte/Desktop/Drives/ds-prog/EM-DATA/gd-for-analysis"
 
-VERSION_NUMBER = "1.08.02"
+VERSION_NUMBER = "1.08.03"
 
 DJANGO_DEV_PORT = "8001"
 
